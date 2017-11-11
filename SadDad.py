301340cdc8a037d5bcfe136a92a7b2efdd0e9f3a
mulcahyr <31449687+mulcahyr@users.noreply.github.com>
2017-11-11 17:40:31 -0600

    Add files via upload

--- SadDad.py
+++ SadDad.py
@@ -13,20 +13,20 @@ class SadDad(Character):
     def question1(self):
         question = "Hey, do you have any aspirin?"
         resA = "Kids screaming a little too loud?"
-        resB = "Yeah I have a whole bottle in my bag.It's almost empty though."
+        resB = "Yeah I have a whole bottle in my bag.It's almost empty though." ##correct
         resC = "No"
         resD = "I don't believe in pharmaceuticals. Illness is a mental state. Think differently."
 
     def question2(self):
          question = "Do they have coffee here?"
-         resA = "They'd better. I'll die without a cup."
+         resA = "They'd better. I'll die without a cup." ##correct
          resB = "No, I don't think so."
          resC = "You mean like... black? Just black coffee??"
          resD = "Ew I hate coffee."
 
     def question3(self):
         question = "I had to sell my car to get a bigger one when I had kids. My first born i named after the jeep I sold to have him."
-        resA = "I have a jeep."
+        resA = "I have a jeep." ##correct
         resB = "Oh...kay?"
         resC = "I'm not that into cars."
         resD = "You had a jeep? That's so boujee."
@@ -34,7 +34,7 @@ class SadDad(Character):
     def question4(self):
         question = "I could use a piece of pizza right now."
         resA = "Bring him a slice of the Chicago style deep dish."
-        resB = "Bring him a slice of the mac and cheese pizza."
+        resB = "Bring him a slice of the mac and cheese pizza." ##correct
         resC = "Bring him a slice of the buffalo chicken pizza."
         resD = "The buffet is empty. They said they'll refill it in 10 minutes."
 
@@ -42,5 +42,5 @@ class SadDad(Character):
         question = "Damn.. I just got a text that I'm behind on my Khol's card payments."
         resA = "I just had to cancel my JCPenney card."
         resB = "That's really unfortunate."
-        resC = "I get all of my sweaters from Khols!"
+        resC = "I get all of my sweaters from Khols!" ##correct
         resD = "I only shop at Vineyard Vines."
